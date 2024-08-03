81a618d17978b6cf64f2e51c3add85c0f8161146
RuslanBoiko <boyko.ruslan78@gmail.com>
2024-08-02 20:40:30 -0400

    Rest API

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://66a6ddc123b29e17a1a3a133.mockapi.io";
+
+export const addMaterial = async (values) => {
+  const response = await axios.post("/materials", values);
+  return response.data;
+};
+
+export const getMaterials = async () => {
+  const response = await axios.get("/materials");
+  return response.data;
+};
+
+export const deleteMaterial = async (id) => {
+  const response = await axios.delete(`/materials/${id}`);
+  return response.data;
+};
+
+export const updateMaterial = async (fields) => {
+  const response = await axios.put(`/materials/${fields.id}`, fields);
+  return response.data;
+};
