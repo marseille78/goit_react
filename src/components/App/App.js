af1bfa96e8f36ef1ae8bce91cda6773a9e6c663d
RuslanBoiko <boyko.ruslan78@gmail.com>
2024-08-08 15:00:33 -0400

    Phonebook with localStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,14 +6,23 @@ import ContactList from "../ContactList";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
     existContact: false,
+  };
+
+  componentDidMount = () => {
+    const startContacts = localStorage.getItem("bookContacts");
+
+    if (startContacts) {
+      this.setState({ contacts: JSON.parse(startContacts) });
+    }
+  };
+
+  componentDidUpdate = (_, prevState) => {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("bookContacts", JSON.stringify(this.state.contacts));
+    }
   };
 
   getVisibleContacts = () => {
