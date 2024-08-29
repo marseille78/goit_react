a3446b779892d573ce67738a67d044cf7e0a3e6f
RuslanBoiko <boyko.ruslan78@gmail.com>
2024-08-28 21:37:17 -0400

    04_hw-01_feedback_widget

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,58 +1,78 @@
-import { Component } from "react";
+import { useReducer } from "react";
 import FeedbackOptions from "../FeedbackOptions";
 import Statistics from "../Statistics";
 import Notification from "../Notification";
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const TYPES = {
+  good: "good",
+  neutral: "neutral",
+  bad: "bad",
+};
+
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case TYPES.bad:
+      return {...state, bad: state.bad + 1};
+    case TYPES.neutral:
+      return {...state, neutral: state.neutral + 1};
+    case TYPES.good:
+      return {...state, good: state.good + 1};
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((sum, item) => sum + item, 0);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((sum, item) => sum + item, 0);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((state.good / countTotalFeedback()) * 100);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  const changeFeedback = (name) => {
+    dispatch({type: TYPES[name]});
   };
 
-  changeFeedback = (name) => {
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1,
-    }));
-  };
+  const feedback = (
+    <Statistics
+      good={state.good}
+      neutral={state.neutral}
+      bad={state.bad}
+      total={countTotalFeedback()}
+      positivePercentage={countPositiveFeedbackPercentage()}
+    />
+  );
 
-  render() {
-    const feedback = (
-      <Statistics
-        good={this.state.good}
-        neutral={this.state.neutral}
-        bad={this.state.bad}
-        total={this.countTotalFeedback()}
-        positivePercentage={this.countPositiveFeedbackPercentage()}
-      />
-    );
-    const notification = <Notification message="There is no feedback" />;
-    return (
-      <div className="container pt-3 pb-3">
+  const notification = <Notification message="There is no feedback" />;
+
+  return (
+    <div className="container pt-3 pb-3">
         <div className="row mb-3">
           <div className="col">
             <FeedbackOptions
-              options={Object.keys(this.state)}
-              onLeaveFeedback={this.changeFeedback}
+              options={Object.keys(state)}
+              onLeaveFeedback={changeFeedback}
             />
           </div>
         </div>
         <div className="row">
           <div className="col">
-            {this.countTotalFeedback() === 0 ? notification : feedback}
+            {countTotalFeedback() === 0 ? notification : feedback}
           </div>
         </div>
       </div>
-    );
-  }
-}
+  );
+};
 
 export default App;
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,20 +2,20 @@ import PropTypes from "prop-types";
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <ul class="list-group">
-      <li class="list-group-item">
+    <ul className="list-group">
+      <li className="list-group-item">
         Good: <b>{good}</b>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         Neutral: <b>{neutral}</b>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         Bad: <b>{bad}</b>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         Total: <b>{total}</b>
       </li>
-      <li class="list-group-item">
+      <li className="list-group-item">
         Positive feedback: <b>{positivePercentage}%</b>
       </li>
     </ul>
