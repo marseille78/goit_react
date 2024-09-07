a6cb74897854348d410ed9b1b841796e3a27f337
RuslanBoiko <boyko.ruslan78@gmail.com>
2024-09-07 10:32:29 -0400

    Aliev 05 03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,36 @@
-// import { useState } from 'react'
-import { Routes, Route } from "react-router-dom";
-import Layout from "./Layout/Layout";
-// import { nanoid } from 'nanoid'
-// import { Toaster } from 'react-hot-toast'
+import { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-// import Header from './Header/Header'
-// import TestUseMemo from './TestUseMemo/TestUseMemo'
+import Layout from './Layout/Layout'
+import HomePage from './pages/HomePage'
+// import NewsPage from './pages/NewsPage'
+// import ToDoPage from './pages/ToDoPage'
+// import ToDoDetails from './ToDo/ToDoDetails'
 
-import HomePage from "./pages/HomePage";
-import NewsPage from "./pages/NewsPage";
-import ToDoPage from "./pages/TodoPage";
-import ToDoDetails from "./ToDo/ToDoDetails";
-// import ToDoList from './ToDoList/ToDoList'
-// import Modal from './Modal/Modal'
-// import FormLogin from './FormLogin/FormLogin'
-// import Search from './Search/Search'
-// import ContentInfo from './ContentInfo/ContentInfo'
-// import Counter from './Counter/Counter'
+const LoginPage = lazy(() => import('./pages/LoginPage'))
+const ToDoDetails = lazy(() => import('./ToDo/ToDoDetails'))
+const ToDoPage = lazy(() => import('./pages/ToDoPage'))
+const NewsPage = lazy(() => import('./pages/NewsPage'))
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="news" element={<NewsPage />} />
-        <Route path="todo" element={<ToDoPage />} />
-        <Route path="todo/:id/:title" element={<ToDoDetails />} />
-      </Route>
-    </Routes>
-  );
-};
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<HomePage />} />
+				<Route path='news' element={<NewsPage />} />
+				<Route path='todo' element={<ToDoPage />} />
+				<Route path='todo/:id' element={<ToDoDetails />} />
+			</Route>
+			<Route
+				path='/login'
+				element={
+					<Suspense>
+						<LoginPage />
+					</Suspense>
+				}
+			/>
+		</Routes>
+	)
+}
 
-export default App;
+export default App
